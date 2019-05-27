87a7cfa57608f699c17df96c1bd6794d374781ea
Franco <fbrusatti@dc.exa.unrc.edu.ar>
2019-05-27 08:58:19 -0300

    Add odt template

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -18,10 +18,17 @@ class StaticPagesController < ApplicationController
   end
 
   def download_paper_format
+    file_hash = {
+      'LaTeX2e' => 'LaTeX2e.zip',
+      'Office2007' => 'Office2007.zip',
+      'word-97-2003' => 'word-97-2003.zip',
+      'templateLNCS' => 'templateLNCS.odt'
+    }
     filename = params[:id]
+
     send_file(
-      "#{Rails.root}/app/assets/images/CFP-CACIC2019.pdf",
-      filename: "#{filename}.zip",
+      "#{Rails.root}/app/assets/images/paper-format/#{file_hash.fetch(filename)}",
+      filename: filename,
       type: 'application/zip')
   end
 
