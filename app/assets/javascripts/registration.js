087be1e9fa4525f239d25783d567310e21bd158a
Santi Depetris <depetris.santi@gmail.com>
2019-08-01 15:58:50 -0300

    Add validation at form in registration page

---
+++ app/assets/javascripts/registration.js
@@ -0,0 +1,46 @@
+$( document ).ready(function() {
+	$('#register-send').on('click', function(){
+		$('#regForm').validate
+		({
+			rules:
+			{
+				registerSurname:
+				{
+				 required: true,
+				},
+				registerName:
+				{
+					required: true,
+				},
+				registerDni:
+				{
+					required: true
+				},
+				registerEmail:
+				{
+					required: true,
+					email: true
+				}
+			},
+			messages:
+			{
+				registerSurname:
+				{
+					required: 'Su apellido es requerido.',
+				},
+				registerName: {
+					required: 'Su nombre es requierido.',
+				},
+				registerDni:
+				{
+					required: 'Su numero de documento es requerido.'
+				},
+				registerEmail:
+				{
+					required: 'Su email es requerido.',
+					email: 'Ingrese una dirección de correo valida.'
+				}
+			}
+		});
+	});
+});
