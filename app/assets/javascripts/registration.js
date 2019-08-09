925dd8866f2ff9cc1049805386c74b74197ff5b2
Santi Depetris <depetris.santi@gmail.com>
2019-08-08 22:58:49 -0300

    Add new fields at form in register page and his respective validation

--- app/assets/javascripts/registration.js
+++ app/assets/javascripts/registration.js
@@ -20,6 +20,22 @@ $( document ).ready(function() {
 				{
 					required: true,
 					email: true
+				},
+				registerPais:
+				{
+					required: true
+				},
+				registerProvincia:
+				{
+					required: true
+				},
+				registerCiudad:
+				{
+					required: true
+				},
+				registerRadio:
+				{
+					required: true
 				}
 			},
 			messages:
@@ -39,7 +55,36 @@ $( document ).ready(function() {
 				{
 					required: 'Su email es requerido.',
 					email: 'Ingrese una dirección de correo valida.'
+				},
+				registerPais:
+				{
+					required: 'Su pais es necesario.'
+				},
+				registerProvincia:
+				{
+					required: 'Su provincia es necesario.'
+				},
+				registerCiudad:
+				{
+					required: 'Su ciudad es necesario.'
+				},
+				registerRadio:
+				{
+					required: 'El curso que va a tomar es necesario.'
 				}
+			},
+			groups:
+			{
+				course: "registerRadio"
+			},
+			errorPlacement: function(error, element){
+				if (element.attr('name') == "registerRadio") {
+				error.insertAfter('#errorCourse');
+				}
+				else{
+					error.insertAfter(element)
+				}
+
 			}
 		});
 	});
